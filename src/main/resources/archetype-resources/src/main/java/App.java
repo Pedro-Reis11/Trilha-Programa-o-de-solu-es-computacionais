dd85dde97f07bce140eba5bb900eefd6c5ab22e0
Pedro-Reis11 <pedroverissimoreis@gmail.com>
2025-11-13 17:50:22 -0300

    Criação das pastas e configuração da IDE

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.gestao.projetos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
